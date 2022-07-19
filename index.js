ef17893360c811033fd3b37371b3b1aae96f6a09
elizabeth sky degrate <skydegrate@gmail.com>
2022-07-19 16:05:25 -0400

    commit tree dcf9d307c04bf0f60b42f890f783d6ebac5d902e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+// Code your solution in this file
+const returnFirstTwoDrivers = function(a) {
+    return a.slice(0,2)
+};
+
+const returnLastTwoDrivers = function(a) {
+    return a.slice(-2)
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(num) {
+    return function(fare) {
+        return fare * num;
+    };
+};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = function(drivers, selectFunction) {
+    return selectFunction(drivers);
+};
